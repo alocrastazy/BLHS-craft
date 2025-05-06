8b09d13da7db44ed9c6aa7b0e67baace1143ca47
alocrastazy <164289490+alocrastazy@users.noreply.github.com>
2025-05-05 23:01:09 -0700

    Replace bungee/bungee.jar with the Waterfall 1.19-R0.1-SNAPSHOT build 526

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
-# In main.sh before you launch, overwrite bungee.jar:
-wget -qO bungee/bungee.jar https://ci.md-5.net/job/BungeeCord/lastStableBuild/artifact/bootstrap/target/BungeeCord.jar
+# fetch the matching Waterfall build and overwrite bungee.jar
+wget -qO bungee/bungee.jar \
+  https://papermc.io/api/v2/projects/waterfall/versions/1.19-R0.1-SNAPSHOT/builds/526/downloads/waterfall-1.19-R0.1-SNAPSHOT-526.jar
 
 #!/bin/bash
 echo "Patching BungeeCord to listen on port $PORT…"
