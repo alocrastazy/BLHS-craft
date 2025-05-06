586e7b46169086d381bc16f45af52d4e627e583c
alocrastazy <164289490+alocrastazy@users.noreply.github.com>
2025-05-05 21:14:37 -0700

    Update to have latest BungeeCord jar file.

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
+# In main.sh before you launch, overwrite bungee.jar:
+wget -qO bungee/bungee.jar https://ci.md-5.net/job/BungeeCord/lastStableBuild/artifact/bootstrap/target/BungeeCord.jar
+
 #!/bin/bash
 echo "Patching BungeeCord to listen on port $PORT…"
 # Replace the listener port line in bungee/config.yml
